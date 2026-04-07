82655763ab5a116734aac20fca8587cc7964363a
Sammuel Morales <est.sammuel.morales@unimilitar.edu.co>
2026-04-07 10:26:58 -0500

    implementacion de ordenamiento de seleccion dentro del codigo

--- src/main/java/com/mycompany/ordenamiento/Ordenamiento.java
+++ src/main/java/com/mycompany/ordenamiento/Ordenamiento.java
@@ -43,5 +43,18 @@ public class Ordenamiento {
             }
         }
     }
+    
+    private static void seleccion(int v[]) {
+    for (int i = 0; i < v.length - 1; i++) {
+
+        int min = i;
+
+        for (int j = i + 1; j < v.length; j++) {
+            if (v[j] < v[min]) {
+                min = j;
+            }
+        }
+    }
+}
 
 }
